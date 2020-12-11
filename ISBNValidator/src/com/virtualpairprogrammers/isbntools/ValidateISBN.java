33f6a6d539fb41c79345ff3d2e284a3d6057270b
yang1014-st <yang1014@stthomas.edu>
2020-12-10 23:04:16 -0600

    ISBNNumberEndingInAnXAreValid-StillRed

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -4,28 +4,27 @@ public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
 		
-		
-		if (isbn.length()!=10) {
-			throw new NumberFormatException("ISBN numbes must be 10 digits long");	
-		}
+		if (isbn.length()!=10) {throw new NumberFormatException("ISBN numbes must be 10 digits long");	}
 	
-		
-		
 		int total =0;
 		
-		for (int i =0; i<10; i++) {
+		for (int i =0; i<10; i++) 
+		{
 			
 			if (!Character.isDigit(isbn.charAt(i))) {
 				
 				if (i==9 && isbn.charAt(i)=='X') {
-					//This is OK
+					total = total + 10;
 				}
-				else
-					throw new NumberFormatException("ISBN numbes must be numeric");		
+				else {
+					throw new NumberFormatException("ISBN numbes must be numeric");	
+				}	
 			} 
-			
-			total += isbn.charAt(i) * (10-i);
+			else {total += isbn.charAt(i) * (10-i);
+			}
 		}
+			
+		
 		
 		if (total%11==0) {
 			return true;
