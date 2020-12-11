5da3c8dde69354fd2fcd787635dfa319ed828115
yang1014-st <yang1014@stthomas.edu>
2020-12-10 22:14:12 -0600

    checkAnInvalidISBN-Forgot@Test

--- ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/ValidateISBNTest.java
@@ -7,10 +7,19 @@ import org.junit.Test;
 public class ValidateISBNTest {
 
 	@Test
-	public void checkValidateISBN() {
+	public void checkAValidateISBN() {
 		ValidateISBN validator = new ValidateISBN();
 		boolean result = validator.checkISBN(140449116);// the first expample, does not require 0140449116 to be a string
 		assertTrue(result);
+	}
+	
+	
+	public void checkAnInvalidISBN() {
+		
+		ValidateISBN validator = new ValidateISBN();
+		boolean result = validator.checkISBN(140449117);// the first expample, does not require 0140449116 to be a string
+		assertFalse(result);
+		
 	}
 
 }
