1d013f335f76531ff5002bb69a1cbe605cf94622
yang1014-st <yang1014@stthomas.edu>
2020-12-11 00:08:28 -0600

    testCanGetACorectLocatorCode-first red

---
+++ ISBNValidator/test/com/virtualpairprogrammers/isbntools/StockManagementTests.java
@@ -0,0 +1,14 @@
+package com.virtualpairprogrammers.isbntools;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class StockManagementTests {
+
+	@Test
+	public void testCanGetACorectLocatorCode() {
+		fail("Not yet implemented");
+	}
+
+}
