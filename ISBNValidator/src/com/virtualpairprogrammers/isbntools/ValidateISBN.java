4d5f1f37da37ee822001301126e0b84a9c734ef7
yang1014-st <yang1014@stthomas.edu>
2020-12-10 22:42:04 -0600

    checkISBN-Green

--- ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
+++ ISBNValidator/src/com/virtualpairprogrammers/isbntools/ValidateISBN.java
@@ -3,6 +3,11 @@ package com.virtualpairprogrammers.isbntools;
 public class ValidateISBN {
 
 	public boolean checkISBN(String isbn) {
+		
+		if (isbn.length()!=10) {
+			throw new NumberFormatException("ISBN numbes must be 10 digits long");	
+		}
+		
 		int total =0;
 		
 		for (int i =0; i<10; i++) {
